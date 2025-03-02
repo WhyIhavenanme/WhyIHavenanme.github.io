82c08e7694608288301bebab712960092eb32f05
朱珈德 <493245020@qq.com>
2025-03-02 11:01:18 +0800

    123

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 5500;
 const picDir = path.join(__dirname, 'pic');
 
 app.use(express.static(__dirname));
